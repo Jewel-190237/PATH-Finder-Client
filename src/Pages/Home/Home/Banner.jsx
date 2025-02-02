17604a891c5472bb4a38eacd1ecbd518738405e4
Azhar190238 <azhar73397@gmail.com>
2025-02-02 19:49:31 +0600

    Added nvbar and hero section updated

--- src/Pages/Home/Home/Banner.jsx
+++ src/Pages/Home/Home/Banner.jsx
@@ -5,7 +5,7 @@ import posterImg from '../../../assets/banner/poster.png'
 const Banner = () => {
   const [isHovered, setIsHovered] = useState(false);
   return (
-    <div className="relative h-[500px] sm:h-[650px] md:h-[680px] lg:h-[800px] xl:h-[982px]  mb-20 " >
+    <div className="relative h-[350px] sm:h-[650px] md:h-[680px] lg:h-[800px] xl:h-[982px] md:mb-20" >
      
         <div
             className="w-full h-full bg-cover bg-center -mt-24"
@@ -24,11 +24,11 @@ const Banner = () => {
                     thumbnailAlt="Hero Video"
                 />
                 <div
-                    className={`absolute -bottom-8 sm:bottom-10 md:bottom-16 lg:bottom-20 xl:bottom-40 left-1/2  transform -translate-x-1/2 w-full bg-[rgba(35,7,19,0.30)] backdrop-blur-[30px] text-white p-10 rounded-lg shadow-xl transition-all duration-500 ease-in-out ${
+                    className={ ` hidden sm:block absolute -bottom-8 sm:bottom-10 md:bottom-16 lg:bottom-20 xl:bottom-40 left-1/2  transform -translate-x-1/2 w-full bg-[rgba(35,7,19,0.30)] backdrop-blur-[30px] text-white p-10 rounded-lg shadow-xl transition-all duration-500 ease-in-out ${
                         isHovered ? "opacity-100 scale-100" : "opacity-0 scale-95"
                     }`}
                 >
-                    <div className="bg-[#20010D] max-w-[675px] ">
+                    <div className="bg-[#20010D] p-3  max-w-[675px] ">
                         <h2 className="heading font-bold font-roboto">
                             <span className="text-transparent bg-clip-text bg-[linear-gradient(90deg,#3F3FDE_0.07%,#FF0DEF_32.88%)]">
                                 Experience
